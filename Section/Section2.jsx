275228e00bd21283a275154216f592282f7c7a12
Vanshika <118618760+Vanshikaduggal@users.noreply.github.com>
2024-05-23 11:27:33 +0530

    Add files via upload

---
+++ Section/Section2.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './section.css'
+const Section2 = () => {
+  return (
+    <div className='section2' id='blog'>
+            <img src='src/assets/images/rectangle_1_copy_7.png' className='img1'/>
+      <img src='src/assets/images/rectangle_3_copy.png' className='img2'/>
+      <div className='images'>
+        <img src='src/assets/images/rectangle_1_copy_8.png' className='i1'/>
+        <img src='src/assets/images/rectangle_1_copy_9.png' className='i2'/>
+        <div>
+        <img src='src/assets/images/rectangle_1_copy_20.png' className='i4'/>
+        <h2 className='heading-2'>2.</h2>
+        </div>
+        <div>
+        <img src='src/assets/images/rectangle_1_copy_6.png' className='i3'/>
+        <h2 className='portfolio'>PORTFOLIO</h2>
+        <p className='paragraph'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem officiis earum vero ut dolor saepe iste labore maiores sunt praesentium!</p>
+        <button className='button2'>Read More</button>
+        </div>
+      </div>
+
+      <h3 className='h3'>LOREM IPSUM DOREM</h3>
+      <p className='p2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima molestiae velit sunt officiis similique! Iusto vel perspiciatis commodi quidem error quibusdam cum veniam sed animi quis, facere voluptate nisi eos ipsum, minus praesentium? Reiciendis, ratione deserunt nulla facere labore voluptate pariatur voluptas, eligendi rerum temporibus, fuga dolor esse totam? Voluptate iste deleniti facilis. Quibusdam illo alias aspernatur amet facere dolorum quae dolor, reprehenderit accusamus accusantium temporibus aut est totam delectus!</p>
+
+    </div>
+  )
+}
+
+export default Section2
